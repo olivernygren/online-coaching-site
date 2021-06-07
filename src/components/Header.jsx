a8a4a4df0989429e2302e261251081e01186ee38
Oliver Nygren <oliver.nygren@icloud.com>
2021-06-07 13:22:28 +0200

    nav bar with animation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,38 @@
 import React from 'react'
-import { Link } from 'react-router';
+import { Link } from 'react-router-dom';
+import '../css/header.css'
 
 function Header() {
   return (
     <header>
-      <h2>ONfitness</h2>
+      <h2 className="header-logo">ON<b>fitness</b></h2>
       <nav>
         <ul>
           <li>
-            <Link to='/'>Link</Link>
+            <Link className="link" to='/'>Hem</Link>
           </li>
           <li>
-            <Link to='/'>Link</Link>
+            <Link className="link" to='/'>E-böcker och färdiga program</Link>
           </li>
           <li>
-            <Link to='/'>Link</Link>
+            <Link className="link" to='/'>Online Coaching</Link>
           </li>
           <li>
-            <Link to='/'>Link</Link>
+            <Link className="link" to='/'>Boka konsultation</Link>
           </li>
         </ul>
+        {/* <ul>
+          <li><a className="link" href="/">Home</a></li>
+          <li><a className="link" href="/">Work</a></li>
+          <li><a className="link" href="/">Blog</a></li>
+          <li><a className="link" href="/">Contact</a></li>
+        </ul> */}
       </nav>
+      <div className="header-icons">
+        <button className="log-in-btn">Logga in</button>
+        {/* <i class="fas fa-user"></i> */}
+        <i class="fas fa-shopping-cart"></i>
+      </div>
     </header>
   )
 }
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Header from './Header'
+import { BrowserRouter as Router } from 'react-router-dom'
+import '../css/main.css' 
 
 function Layout() {
   return (
     <>
-      <Header/>
+      <Router>
+        <Header/>
+      </Router>
     </>
   )
 }
