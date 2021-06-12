f35a48fc4540160c7e39e5808ca2893d8f8faa3f
Oliver Nygren <oliver.nygren@icloud.com>
2021-06-12 17:06:45 +0200

    consultation page

--- src/components/Consultation.jsx
+++ src/components/Consultation.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import '../css/main.css'
+import '../css/consultation.css'
 
 function Consultation() {
   return (
-    <div>
-      <h2>consultation</h2>
+    <div className="consultation-root">
+      <div className="consultation-container">
+        <div className="content">
+          <h2>Boka gratis konsultation <i class="fas fa-video"></i> </h2>
+          <div className="text-body">
+            <p>Är du osäker på hur du ska nå dina mål? En gratis konsultation är ett perfekt första steg på vägen dit. Helt gratis och utan risk för dig.</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex minus alias voluptate impedit laudantium est facere ducimus incidunt obcaecati adipisci voluptatum consectetur aliquid nobis ut doloribus, consequatur rerum ea quam.
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum ut deserunt totam iste numquam voluptatibus dignissimos consectetur aut, quaerat rerum adipisci iure, exercitationem officiis laudantium enim ullam, reprehenderit id officia.
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum ut deserunt totam iste numquam voluptatibus dignissimos consectetur aut, quaerat rerum adipisci iure, exercitationem officiis laudantium enim ullam, reprehenderit id officia.
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum ut deserunt totam iste numquam voluptatibus dignissimos consectetur aut, quaerat rerum adipisci iure, exercitationem officiis laudantium enim ullam, reprehenderit id officia.
+            </p>
+          </div>
+          <a href="/">Boka in din konsultation här <i className="fas fa-arrow-right"></i> </a>
+        </div>
+      </div>
     </div>
   )
 }
