7b44f1c1971d02e2cea3028697004c2b989a2107
Oliver Nygren <oliver.nygren@icloud.com>
2021-06-17 13:26:28 +0200

    working multi page form

---
+++ src/formHandler.js
@@ -0,0 +1,9 @@
+const nextBtn1 = document.getElementById('next-btn-1')
+const nextBtn2 = document.getElementById('next-btn-2')
+const nextBtn3 = document.getElementById('next-btn-3')
+const nextBtn4 = document.getElementById('next-btn-4')
+const nextBtn5 = document.getElementById('next-btn-5')
+
+nextBtn1.addEventListener('click', () => {
+  console.log('click')
+})
