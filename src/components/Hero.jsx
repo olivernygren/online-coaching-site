bbf4ac1ef3cf6cc292f17be2d2fa6b087cc984d6
Oliver Nygren <oliver.nygren@icloud.com>
2021-06-30 15:19:05 +0200

    fixed home about me image size issue

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -51,7 +51,7 @@ function Hero() {
     <div className="hero">
       <div className="content">
         <div className="text">
-          <h4>Oliver Nygren - Lic. PT / Online Coach</h4>
+          <h4>Oliver Nygren - Online Coach / Tränings-entusiast</h4>
           <h1>Är du redo att uppnå stora träningsresultat?</h1>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia fugit repellat! Omnis dolorum distinctio maxime debitis ratione suscipit ad similique officiis, necessitatibus excepturi.</p>
           <div className="buttons">
