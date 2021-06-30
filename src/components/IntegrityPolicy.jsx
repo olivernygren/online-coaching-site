47182bdb477fd8d2a5afd7a79dfa919fe7491c05
Oliver Nygren <oliver.nygren@icloud.com>
2021-06-30 13:31:01 +0200

    int policy page done

--- src/components/IntegrityPolicy.jsx
+++ src/components/IntegrityPolicy.jsx
@@ -1,9 +1,43 @@
 import React from 'react'
+import '../css/intPolicy.css'
 
 function IntegrityPolicy() {
   return (
-    <div>
-      <h2>int policy</h2>
+    <div className="int-policy-container">
+      <div className="content">
+
+        <div className="heading">
+          <h2>Integritetspolicy</h2>
+          <span>30 juni 2021</span>
+        </div>
+
+        <h3>Behandling av personuppgifter på ONfitness</h3>
+        <p>För ONfitness är personlig integritet viktigt. Vi eftersträvar en hög nivå av dataskydd. I denna policy förklarar vi hur vi samlar in och använder personuppgifter. Vi beskriver också dina rättigheter och hur du kan göra dem gällande. </p>
+        <p>Du är alltid välkommen att kontakta oss om du har frågor om hur vi behandlar dina personuppgifter. Kontaktuppgifter står sist i denna text. </p>
+
+        <h3>Vad är en personuppgift och vad är en behandling av personuppgifter?</h3>
+        <p>Personuppgifter är alla uppgifter om en levande fysisk person som direkt eller indirekt kan kopplas till den personen. Det handlar inte bara om namn och personnummer utan även om till exempel bilder och e-postadresser.</p>
+        <p>Behandling av personuppgifter är allt som sker med personuppgifterna i IT-systemen, oavsett om det handlar om mobila enheter eller datorer. Det handlar om till exempel insamling, registrering, strukturering, lagring, bearbetning och överföring. I vissa fall kan även manuella register omfattas. </p>
+      
+        <h3>Vilka personuppgifter samlar vi in om dig och varför?</h3>
+        <p>Inga personuppgifter samlas in och/eller sparas ned automatiskt när du besöker hemsidan. Om du fyller i formuläret på hemsidan så inkluderar du ålder, kön, namn, mobilnummer och e-post adress. Denna information sparas ej ned i någon databas eller liknande. Informationen skickas via formulärshanteringsprogrammet som ett email till oss. Detta email utgår vi ifrån när vi ska ta kontakt med dig.</p>
+      
+        <h3>Personuppgifter som anges vid betalning</h3>
+        <p>Alla personuppgifter som anges vid betalning och köp av en produkt hanteras av betaltjänsten Stripe. Du kan läsa deras policy <a href="https://stripe.com/en-se/privacy">här</a>.</p>
+      
+        <h3>Aktörer som är självständigt personuppgiftsansvariga </h3>
+        <p>Vi kan även dela dina personuppgifter med vissa andra aktörer som är självständigt personuppgiftsansvariga, till exempel myndigheter som Skatteverket, när vi är skyldiga att lämna ut sådana uppgifter med stöd av lag eller myndighetsbeslut. När dina personuppgifter delas med en aktör som är självständigt personuppgiftsansvarig gäller den organisationens integritetspolicy och personuppgiftshantering.</p>
+      
+        <h3>Hur länge sparar vi dina personuppgifter? </h3>
+        <p>Vi sparar aldrig dina personuppgifter längre än vad som är nödvändigt för respektive ändamål.</p>
+
+        <h3>Vad är dina rättigheter som registrerad? </h3>
+        <p>Som registrerad har du enligt gällande lagstiftning ett antal rättigheter. Du har rätt till att få ett utdrag som visar vilka personuppgifter vi har registrerade om dig. Du kan begära rättelse av felaktiga uppgifter och i vissa fall radering.</p>
+      
+        <h3>Kontakta oss vid frågor om hur vi behandlar personuppgifter.</h3>
+        <p>Om du har frågor om hur vi behandlar personuppgifter kontakta oliver@onfitness.se. </p>
+        <p>Vi kan komma att göra ändringar i vår integritetspolicy. Den senaste versionen av integritetspolicyn finns alltid här på webbplatsen.</p>
+      </div>
     </div>
   )
 }
