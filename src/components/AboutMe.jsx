0a03d878772d549e3b540c8fd25d02f530511100
Oliver Nygren <oliver.nygren@icloud.com>
2021-07-06 10:51:00 +0200

    about me responsive + small fixes

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react'
 import '../css/main.css'
 import '../css/aboutMe.css'
 import Me from '../images/me-cropped-with-icon.png'
+import MeAlt from '../images/me-cropped-2.jpg'
+import Logo from '../images/ONfitness-light.svg'
 
 function AboutMe() {
 
@@ -37,9 +39,17 @@ function AboutMe() {
             </div>
           </div>
         </div>
+
+
         <div className="text">
+          <img className="about-me-mobile-img" src={MeAlt} alt="Bild på mig" />
           <h2>Vem är jag?</h2>
-          <h4> <b>Oliver Nygren</b>, Grundare, ONfitness</h4>
+          <h4> <b>Oliver Nygren </b>, Grundare, <img src={Logo} alt="ONfitness Logo" /> </h4>
+          <div className="mobile-h4">
+            <b>Oliver Nygren</b>
+            <p>Grundare</p>
+            <img src={Logo} alt="ONfitness Logo" />
+          </div>
           <p>
             Mitt namn är Oliver Nygren och det är jag som är grundare av ONfitness. Jag är idag 20 år gammal och har tränat sedan 15 års ålder. Under dessa
             fem år har jag levt totalt inkapslad i fitness-bubblan. Jag har spenderat timme efter timme, dag efter dag sedan dess på att lära mig allt som går 
@@ -50,6 +60,7 @@ function AboutMe() {
             helt och hållet och startade upp ONfitness, en plattform där jag kan jag göra just detta. Jag driver företaget helt själv med allt vad det innebär.
           </p>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, praesentium eveniet voluptates velit commodi iste, temporibus non nostrum similique, eum veritatis reiciendis obcaecati a alias rerum eius? Deserunt, doloremque alias.</p>
+          <img className="about-me-mobile-img-2" src={MeAlt} alt="Bild på mig" />
         </div>
       </div>
     </div>
