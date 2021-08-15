86fc622d4ae109ee073e58feb4c1cbbc17056add
Oliver Nygren <oliver.nygren@icloud.com>
2021-08-15 20:46:02 +0200

    cards products fix

--- src/components/products/Program1.jsx
+++ src/components/products/Program1.jsx
@@ -30,20 +30,21 @@ function Program1() {
   const relatedProduct1Link = products[relatedProduct1ID].link
   const relatedProduct1Cover = products[relatedProduct1ID].coverImage
   
-  // const relatedProduct2ID = products.findIndex((product) => product.id === 2001)
-  // const relatedProduct2Name = products[relatedProduct2ID].name
-  // const relatedProduct2Price = products[relatedProduct2ID].price
-  // const relatedProduct2Link = products[relatedProduct2ID].link
+  const relatedProduct2ID = products.findIndex((product) => product.id === 1002)
+  const relatedProduct2Name = products[relatedProduct2ID].name
+  const relatedProduct2Price = products[relatedProduct2ID].price
+  const relatedProduct2Link = products[relatedProduct2ID].link
+  const relatedProduct2Cover = products[relatedProduct2ID].coverImage
 
   const redirectToRelatedProduct1 = (e) => {
     e.preventDefault()
     history.push(relatedProduct1Link)
   }
 
-  // const redirectToRelatedProduct2 = (e) => {
-  //   e.preventDefault()
-  //   history.push(relatedProduct2Link)
-  // }
+  const redirectToRelatedProduct2 = (e) => {
+    e.preventDefault()
+    history.push(relatedProduct2Link)
+  }
 
   return (
     <div className="product-container">
@@ -69,7 +70,7 @@ function Program1() {
               <li> <b>+</b> Allt basinfo du behöver veta kring hur du ska träna och tänka kring träning som nybörjare</li>
               <li> <b>+</b> Kortare introduktion till hur ska äta för att både bygga muskler och tappa fett</li>
               <li> <b>+</b> Guide till hur du kan gå vidare och fortsätta utvecklas efter programmet</li>
-              <li> <b>+</b> 31 sidor fulla av information och utan onödiga utfyllnader!</li>
+              <li> <b>+</b> 34 sidor fulla av information och utan onödiga utfyllnader!</li>
             </ul>
             <p className="delivery-notice">Notera att leverans av program och e-böcker kan ta upp till 24h efter betalning!</p>
             <a className="buy-now-btn" href={productPaymentLink} target="blank">Köp nu <i className="fas fa-arrow-right"></i> </a>
@@ -95,7 +96,7 @@ function Program1() {
                 <button className="add-to-cart-btn" onClick={redirectToRelatedProduct1}>Läs mer <i className="fas fa-arrow-right"></i> </button>
               </div>
             </div>
-            {/* <div className="related-product related-product-2" onClick={redirectToRelatedProduct2}>
+            <div className="related-product related-product-2 disabled-product" onClick={redirectToRelatedProduct2}>
               <div className="program program-2">
                 <div className="program-header-btns">
                   <i className="fas fa-eye" title="Läs mer"></i>
@@ -103,12 +104,12 @@ function Program1() {
                   <i className="fas fa-arrow-right" title="Läs mer"></i>
                 </div>
                 <div className="img-bg"></div>
-                <img className="program-cover-img" src={Cover} alt="Program" />
+                <img className="program-cover-img" src={relatedProduct2Cover} alt="Program" />
                 <h4>{relatedProduct2Name}</h4>
                 <h5>{relatedProduct2Price} kr</h5>
-                <button className="add-to-cart-btn" onClick={redirectToRelatedProduct2}>Läs mer <i className="fas fa-arrow-right"></i> </button>
+                <button className="add-to-cart-btn" onClick={redirectToRelatedProduct2}>KOMMER SNART <i className="fas fa-arrow-right"></i> </button>
               </div>
-            </div> */}
+            </div>
           </div>
         </div>
       </div>
